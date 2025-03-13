6397a6313afd5167ad0b8243953a8a67e8e646ad
xFlamez <fireball.sui@gmail.com>
2025-03-13 15:34:13 -0500

    Test
    test

---
+++ src/main/java/devv/rbfz/entityBuilder/EntityBuilder.java
@@ -0,0 +1,17 @@
+package devv.rbfz.entityBuilder;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EntityBuilder extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
